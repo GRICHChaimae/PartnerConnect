271d533c5fb26fd3757c9e2950a668b94f434657
GRICHChaimae <93976924+GRICHChaimae@users.noreply.github.com>
2023-03-25 19:14:10 +0100

    Mentee login front-end

--- PartnerConnec-dev/api/src/protégé/menteeLogin/dto/mentee-login.dto.ts
+++ PartnerConnec-dev/api/src/protégé/menteeLogin/dto/mentee-login.dto.ts
@@ -1,12 +1,10 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class MenteeLoginDto {
   @IsNotEmpty()
-  @IsEmail({}, { message: 'Please enter correct email' })
   readonly email: string;
 
   @IsNotEmpty()
   @IsString()
-  @MinLength(6)
   readonly password: string;
 }
